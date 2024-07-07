9f9ffcb9fabfb7d3ab6f3719757712e2ec178fa9
I'm-KakaRoT <110683430+ARDHENDU1234@users.noreply.github.com>
2024-07-07 10:55:23 +0530

    Delete drone-web-app-main/drone-web-app-main directory

--- drone-web-app-main/drone-web-app-main/frontend/peryton/src/pages/blogs/BlogCategory.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-const BlogCategory = () => {
-  const { category } = useParams();
-  return <div>{category}</div>;
-};
-
-export default BlogCategory;
